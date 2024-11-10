f48d8150ae06f740fae4fc371666f46e81b0259a
Abhishek <pws@PWSBLRPW395.local>
2024-11-10 23:13:03 +0530

    sidebar has been added succesfully

--- src/components/DashboardSidebar.vue
+++ src/components/DashboardSidebar.vue
@@ -1,23 +1,48 @@
 <template>
-  <v-navigation-drawer app>
-    <v-list>
-      <v-list-item to="/" exact>
-        <v-list-item-title>Dashboard</v-list-item-title>
-      </v-list-item>
-      <v-list-item >
-        <v-list-item-title>Reports</v-list-item-title>
-      </v-list-item>
-      <v-list-item >
-        <v-list-item-title>Settings</v-list-item-title>
-      </v-list-item>
-    </v-list>
+  <v-navigation-drawer app color="primary" dark>
+    <v-container fluid>
+      <v-row class="d-flex align-center">
+        <v-col>
+          <v-avatar size="40">
+            <v-img src="/Users/pws/vue-js/vuejs-vuetify-grid-poc/src/assets/images/download.png" alt="Avatar" />
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <span class="text-h6">Test Organisation</span>
+        </v-col>
+      </v-row>
+
+      <v-divider class="my-4"></v-divider>
+
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-clipboard-text</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Projects</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-container>
   </v-navigation-drawer>
 </template>
 
-<script setup>
-// Any logic for the sidebar can go here
+<script>
+export default {
+  name: 'DashboardSidebar',
+};
 </script>
 
 <style scoped>
-/* Add any specific styles for the sidebar */
+/* Additional styling if needed */
 </style>
